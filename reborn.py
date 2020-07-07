9eb131fa77f6fce8d708f4c579d8d1bcc85fc3a3
Don Kim <donkim1153@gmail.com>
2020-07-06 20:15:02 -0400

    adding chen's changes

--- reborn.py
+++ reborn.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from pytz import timezone
 import pytz
 import discord
@@ -18,14 +18,13 @@ cwkpqId = 711792105361637457
 htId = 712393075699220482
 apqId = 711765855125373020
 
-timezones = ['America/New_York', 'America/Los_Angeles', 'Singapore', 'Israel']
+timezones = [
+	'US/Eastern',
+	'US/Pacific',
+	'Asia/Singapore',
+	'Israel'
+]
 
-timezoneDict = {
-	"ETC": "America/New_York",
-	"PST": "America/Los_Angeles", 
-	"SGT": "Singapore",
-	"IST": "Israel"
-}
 @bot.event
 async def on_ready():
 	print('bot is ready')
@@ -39,20 +38,18 @@ async def commandList(ctx):
 	#embed.add_field(name=".ht" value="Sets a horntail run. Parameters taken is a time in UTC.\n ex of usage: .ht 0:00", inline=False)
 	#embed.add_field(name=".apq" value="Sets a apq run. Parameters taken is a time in UTC.\n ex of usage: .apq 0:00", inline=False)
 	await ctx.send("Help\n" +
-		".zakum: Sets a zakum run. Parameters taken is a time in UTC.\n ex of usage: .zakum 0:00\n\n" + 
+		".zakum: Sets a zakum run. Parameters taken is a time in UTC.\n ex of usage: .zakum 0:00\n\n" +
 		".scarga: Sets a scarga run. Parameters taken is a time in UTC.\n ex of usage: .scarga 0:00\n\n" +
 		".cwkpq: Sets a cwkpq run. Parameters taken is a time in UTC.\n ex of usage: .cwkpq 0:00\n\n" +
 		".ht: Sets a horntail run. Parameters taken is a time in UTC.\n ex of usage: .ht 0:00\n\n" +
 		".apq: Sets a apq run. Parameters taken is a time in UTC.\n ex of usage: .apq 0:00")
 
 @bot.command()
-async def zakum(ctx, inputTime, timeZone):
-	if (validateInput(inputTime, timeZone) == False):
-		await ctx.send("Please ensure your inputs are in the format time=##:## timezone=[ETC,PST,SGT,IST]")
-		raise
+async def zakum(ctx, inputTime):
 	channel = bot.get_channel(eventChannelId)
-	convertedTimeString = convertedMultipleTz(inputTime, timeZone)
-	message = await channel.send(f'{rebornHeader}\n<@&{zakumId}> scheduled for \n{convertedTimeString}' +
+	localized_times = utcToLocalizedTzs(inputTime)
+	message = await channel.send(f'{rebornHeader}\n<@&{zakumId}> scheduled for {inputTime} UTC.\n\n' +
+		localized_times +
 		'Please react with the following to sign up:\n' +
 		':regional_indicator_b: Bishop\n' +
 		':regional_indicator_r: Ranged\n' +
@@ -68,7 +65,9 @@ async def zakum(ctx, inputTime, timeZone):
 @bot.command()
 async def scarga(ctx, inputTime):
 	channel = bot.get_channel(eventChannelId)
-	message = await channel.send(f'{rebornHeader}\n<@&{scargaId}> scheduled for {inputTime} UTC.\n\n' + 
+	localized_times = utcToLocalizedTzs(inputTime)
+	message = await channel.send(f'{rebornHeader}\n<@&{scargaId}> scheduled for {inputTime} UTC.\n\n' +
+		localized_times +
 		'Please react with the following to sign up:\n' +
 		':regional_indicator_b: Bishop\n' +
 		':regional_indicator_r: Ranged\n' +
@@ -85,7 +84,9 @@ async def scarga(ctx, inputTime):
 @bot.command()
 async def cwkpq(ctx, inputTime):
 	channel = bot.get_channel(eventChannelId)
-	message = await channel.send(f'{rebornHeader}\n<@&{cwkpqId}> scheduled for {inputTime} UTC.\n\n' + 
+	localized_times = utcToLocalizedTzs(inputTime)
+	message = await channel.send(f'{rebornHeader}\n<@&{cwkpqId}> scheduled for {inputTime} UTC.\n\n' +
+		localized_times +
 		'Please react with the following to sign up:\n' +
 		':regional_indicator_a: Archer\n' +
 		':regional_indicator_w: Warrior\n' +
@@ -112,7 +113,9 @@ async def cwkpq(ctx, inputTime):
 @bot.command()
 async def ht(ctx, inputTime):
 	channel = bot.get_channel(eventChannelId)
-	message = await channel.send(f'{rebornHeader}\n<@&{htId}> scheduled for {inputTime} UTC.\n\n' + 
+	localized_times = utcToLocalizedTzs(inputTime)
+	message = await channel.send(f'{rebornHeader}\n<@&{htId}> scheduled for {inputTime} UTC.\n\n' +
+		localized_times +
 		'Please react with the following to sign up:\n' +
 		':regional_indicator_b: Bishop\n' +
 		':regional_indicator_s: Seduce\n' +
@@ -131,7 +134,9 @@ async def ht(ctx, inputTime):
 @bot.command()
 async def apq(ctx, inputTime):
 	channel = bot.get_channel(eventChannelId)
-	message = await channel.send(f'{rebornHeader}\n<@&{apqId}> scheduled for {inputTime} UTC.\n\n' + 
+	localized_times = utcToLocalizedTzs(inputTime)
+	message = await channel.send(f'{rebornHeader}\n<@&{apqId}> scheduled for {inputTime} UTC.\n\n' +
+		localized_times +
 		'Please react with the following to sign up:\n' +
 		':regional_indicator_b: Bride\n' +
 		':regional_indicator_g: Groom'
@@ -144,16 +149,22 @@ def validateInput(stringTime, timeZone):
 	time_re = re.compile(r'^\d?\d:\d\d$')
 	return True if time_re.match(stringTime) and timezoneDict.get(timeZone) != None else False
 
-def convertedMultipleTz(inputTime, timeZone):
-	templateStr = "{0}:{1} {2}\n"
-	dict = copy.deepcopy(timezoneDict)
-	timeObj = datetime.strptime(inputTime, "%H:%M")
-	localTimeObj = pytz.timezone(timezoneDict.get(timeZone)).localize()
-	convStr = templateStr.format(localTimeObj.hour, localTimeObj.minute, timeZone)
-	for curTz in timezoneDict:
-		if(curTz != timeZone):
-			curDateTime=localTimeObj.asTimeZone(pytz.timezone(timezoneDict.get(curTz)))
-			convStr = convStr + curDateTime.format(curDateTime.hour, curDateTime.minute, curTz)
-	return convStr
+def utcToLocalizedTzs(inputTime):
+	input_time_obj = datetime.strptime(inputTime, '%H:%M')
+	time_now = datetime.now(tz=pytz.utc) # offset aware datetime
+	scheduled_utc_time = time_now.replace(
+			hour=input_time_obj.hour,
+			minute=input_time_obj.minute,
+			second=0,
+			microsecond=0,
+		)
+	# assume scheduled time needs to be in the future
+	if scheduled_utc_time < time_now:
+		scheduled_utc_time + timedelta(days=1)
+
+	localizedTimes = [
+		('• ' + scheduled_utc_time.astimezone(pytz.timezone(tz)).strftime('%H:%M') + ' ' + tz) for tz in timezones
+	]
+	return '**Localized Timezones:** \n' + "\n".join(localizedTimes) + '\n\n'
 
 bot.run(botToken)
