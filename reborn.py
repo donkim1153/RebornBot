0dfdc9a1b2a13d6e69a9bcbb19dc2024f1bd2fda
Christopher Liu <liuchri@umich.edu>
2020-07-16 18:00:09 -0400

    Bandaid fix for current date, added input validation

--- reborn.py
+++ reborn.py
@@ -69,6 +69,9 @@ async def commandList(ctx):
 
 @bot.command()
 async def zakum(ctx, inputTime, inputDate=str(date.today())[5:]):
+	if (validateInput(inputTime, inputDate) == False):
+		await ctx.send("Please ensure your inputs are in the format time=##:##, date=MM-DD (date is optional)")
+		raise Exception("Invalid input")
 	channel = bot.get_channel(eventChannelId)
 	localized_times = utcToLocalizedTzs(inputTime, inputDate)
 	time_object = generateTimeObject(inputTime, inputDate)
@@ -93,6 +96,9 @@ async def zakum(ctx, inputTime, inputDate=str(date.today())[5:]):
 
 @bot.command()
 async def scarga(ctx, inputTime, inputDate=str(date.today())[5:]):
+	if (validateInput(inputTime, inputDate) == False):
+		await ctx.send("Please ensure your inputs are in the format time=##:##, date=MM-DD (date is optional)")
+		raise Exception("Invalid input")
 	channel = bot.get_channel(eventChannelId)
 	localized_times = utcToLocalizedTzs(inputTime, inputDate)
 	time_object = generateTimeObject(inputTime, inputDate)
@@ -116,6 +122,9 @@ async def scarga(ctx, inputTime, inputDate=str(date.today())[5:]):
 
 @bot.command()
 async def cwkpq(ctx, inputTime, inputDate=str(date.today())[5:]):
+	if (validateInput(inputTime, inputDate) == False):
+		await ctx.send("Please ensure your inputs are in the format time=##:##, date=MM-DD (date is optional)")
+		raise Exception("Invalid input")
 	channel = bot.get_channel(eventChannelId)
 	localized_times = utcToLocalizedTzs(inputTime, inputDate)
 	time_object = generateTimeObject(inputTime, inputDate)
@@ -149,6 +158,9 @@ async def cwkpq(ctx, inputTime, inputDate=str(date.today())[5:]):
 
 @bot.command()
 async def ht(ctx, inputTime, inputDate=str(date.today())[5:]):
+	if (validateInput(inputTime, inputDate) == False):
+		await ctx.send("Please ensure your inputs are in the format time=##:##, date=MM-DD (date is optional)")
+		raise Exception("Invalid input")
 	channel = bot.get_channel(eventChannelId)
 	localized_times = utcToLocalizedTzs(inputTime, inputDate)
 	time_object = generateTimeObject(inputTime, inputDate)
@@ -174,6 +186,9 @@ async def ht(ctx, inputTime, inputDate=str(date.today())[5:]):
 
 @bot.command()
 async def apq(ctx, inputTime, inputDate=str(date.today())[5:]):
+	if (validateInput(inputTime, inputDate) == False):
+		await ctx.send("Please ensure your inputs are in the format time=##:##, date=MM-DD (date is optional)")
+		raise Exception("Invalid input")
 	channel = bot.get_channel(eventChannelId)
 	localized_times = utcToLocalizedTzs(inputTime, inputDate)
 	time_object = generateTimeObject(inputTime, inputDate)
@@ -192,6 +207,9 @@ async def apq(ctx, inputTime, inputDate=str(date.today())[5:]):
 	
 @bot.command()
 async def gpq(ctx, inputTime, inputDate=str(date.today())[5:]):
+	if (validateInput(inputTime, inputDate) == False):
+		await ctx.send("Please ensure your inputs are in the format time=##:##, date=MM-DD (date is optional)")
+		raise Exception("Invalid input")
 	channel = bot.get_channel(eventChannelId)
 	localized_times = utcToLocalizedTzs(inputTime, inputDate)
 	time_object = generateTimeObject(inputTime, inputDate)
@@ -243,7 +261,7 @@ def generateTimeObject(inputTime, inputDate):
 			microsecond=0,
 		)
 	# assume scheduled time needs to be in the future
-	if scheduled_utc_time < time_now:
+	while scheduled_utc_time < time_now:
 		scheduled_utc_time = scheduled_utc_time + timedelta(days=1)
 	
 	#print(scheduled_utc_time, flush=True)
@@ -256,6 +274,13 @@ def utcToLocalizedTzs(inputTime, inputDate):
 		('• ' + scheduled_utc_time.astimezone(pytz.timezone(tz)).strftime('%m/%d - %H:%M').lstrip('0').replace('/0', '/') + ' ' + tz) for tz in timezones
 	]
 	return '**Localized Timezones:** \n' + "\n".join(localizedTimes) + '\n\n'
+	
+def validateInput(inputTime, inputDate):
+	print('validating string', flush=True);
+	time_re = re.compile(r'^\d?\d:\d\d$')
+	date_re = re.compile(r'\d{1,2}-\d{1,2}')
+	
+	return True if time_re.match(inputTime) and date_re.match(inputDate) else False
 
 async def checkEvents():
 	await bot.wait_until_ready()
